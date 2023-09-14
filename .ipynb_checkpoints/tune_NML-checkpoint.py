42a8ffa2ec702435686afdddcc15c8c8f3c8a3b5
Majd Ghrear <majd@gpu-0016.hpc.ci.its.hawaii.edu>
2023-09-14 22:32:46 +0000

    modified labels in test_models.ipynb

--- .ipynb_checkpoints/tune_NML-checkpoint.py
+++ .ipynb_checkpoints/tune_NML-checkpoint.py
@@ -7,9 +7,9 @@ import mymodels
 ########################################
 ########################################
 # Select Energy (40 or 50 keV)
-Energy = 50
+Energy = 40
 # Select diff amount (H or L)
-Diff = 'H'
+Diff = 'L'
 # Slecet cheat parameter True or False
 cheat = False
 ########################################
@@ -36,9 +36,21 @@ NML_info["positions"] = NML_info.apply(lambda row: torch.load('/home/majd/sparse
 NML_info["charges"] = NML_info.apply(lambda row: torch.load('/home/majd/sparse_testing_tensors_'+str(Energy)+'keV_'+Diff+'diff/'+ 'sparse_recoils_' + str(row['index']) + '.pt').type(torch.FloatTensor).coalesce().values().flatten().numpy()*1.0, axis=1)
 
 # Do a grid search for the best parameters
-for wo in np.arange(0.04,0.50,0.02):
-    for n_sigma_L in np.arange(1.4,5.0,0.2):
-        for delta_sigma in np.arange(1.4,5.0,0.2):
+# for wo in np.arange(0.04,0.50,0.02):
+#     for n_sigma_L in np.arange(1.4,5.0,0.2):
+#         for delta_sigma in np.arange(1.4,5.0,0.2):
+
+# Coarse search settings
+wos = np.arange(0.02,2.0,0.2)
+wos = np.append(wos,[1e999]) # add wo = inf to the end of the array, this tests the case where we do not re-weight the points with exp(-dist(x_i,x_ip)/wo)
+n_sigma_Ls = np.arange(1.0,8.0,0.5)
+delta_sigmas = np.arange(1.0,15.0,0.5)
+
+cnt = 0
+
+for wo in wos:
+    for n_sigma_L in n_sigma_Ls:
+        for delta_sigma in delta_sigmas:
             
             n_sigma_H = n_sigma_L + delta_sigma
             
@@ -48,6 +60,8 @@ for wo in np.arange(0.04,0.50,0.02):
             NML_Loss_temp = mytools.CSloss(torch.Tensor(v_pred_temp), torch.Tensor(v_true_temp)).item()
             
             df_tune = df_tune.append({ 'wo' : wo, 'n_sigma_L' :  n_sigma_L, 'n_sigma_H' : n_sigma_H, 'Loss' : NML_Loss_temp, 'eff' : NML_eff_temp }, ignore_index = True)
-            df_tune.to_pickle('../tune_NML_model/tune_NML_'+str(Energy)+'keV_'+Diff+'diff_cheat-'+str(cheat)+'.pk')
+            df_tune.to_pickle('../tune_NML_model_coarse/tune_NML_'+str(Energy)+'keV_'+Diff+'diff_cheat-'+str(cheat)+'.pk')
+            print(cnt, "/", len(wos)*len(n_sigma_Ls)*len(delta_sigmas) )
             print('wo: ', wo, ' n_sigma_L: ',  n_sigma_L, ' n_sigma_H: ', n_sigma_H, ' Loss: ', NML_Loss_temp, ' eff: ', NML_eff_temp )
+            cnt += 1
             
